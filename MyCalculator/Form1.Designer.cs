a00da8c5fc2fdf9dc55918aa8cd5a1d15133d19c
suchita.chinchvilkar <37504073+suchita-ch@users.noreply.github.com>
2021-10-27 00:21:12 +0530

    Add project files.

---
+++ MyCalculator/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MyCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textResult
+            // 
+            this.textResult.Enabled = false;
+            this.textResult.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResult.Location = new System.Drawing.Point(12, 41);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(361, 38);
+            this.textResult.TabIndex = 0;
+            this.textResult.Text = "0";
+            this.textResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(12, 100);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(63, 54);
+            this.btnOne.TabIndex = 1;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(90, 100);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(63, 54);
+            this.btnTwo.TabIndex = 2;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(159, 100);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(63, 54);
+            this.btnThree.TabIndex = 3;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(228, 181);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(63, 54);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Tag = "";
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(310, 181);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(63, 54);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(228, 100);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(63, 54);
+            this.btnDivide.TabIndex = 6;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.Location = new System.Drawing.Point(310, 100);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(63, 54);
+            this.btnEqual.TabIndex = 7;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(400, 391);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.textResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEqual;
+    }
+}
+
---
+++ MyCalculator/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalculator
+{
+    public partial class Calculator : Form
+    {
+        List<string> equation = new List<string>();
+
+        string tempnumber;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void addSign(string sign)
+        {
+            if (tempnumber != string.Empty)
+            {
+                equation.Add(tempnumber);
+                equation.Add(sign);
+                tempnumber = "";
+                textResult.Text += sign;
+            }
+        }
+        private void addNumber(string number)
+        {
+            tempnumber += number;
+            if(textResult.Text=="0")
+                textResult.Text = number;
+            else
+                textResult.Text += number;
+        }
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            addNumber("1");
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            addNumber("2");
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            addNumber("3");
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (textResult.Text.Trim() == string.Empty || textResult.Text.Trim() == "0")
+                MessageBox.Show("Please enter valid value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                addSign("+");
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (textResult.Text.Trim() == string.Empty || textResult.Text.Trim() == "0")
+                MessageBox.Show("Please enter valid value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                addSign("/");
+        }
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+                if (textResult.Text.Trim() == string.Empty || textResult.Text.Trim() == "0")
+                MessageBox.Show("Please enter valid value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                if (tempnumber != string.Empty)
+                    equation.Add(tempnumber);
+                int index = 1;
+                decimal result = 0;
+                string sign = "+";
+
+                foreach (string part in equation)
+                {
+                    if (index % 2 != 0)
+                    {
+                        if (sign == "+")
+                            result = result + Convert.ToDecimal(part);
+                        else if (sign == "/")
+                            result = result / Convert.ToDecimal(part);
+                        else if(sign=="*")
+                            result = result * Convert.ToDecimal(part);
+                    }
+                    else
+                    {
+                        sign = part;
+                    }
+                    index++;
+                }
+                textResult.Text = result.ToString();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            equation.Clear();
+            textResult.Text = "0";
+        }
+
+        private void CalculateResult()
+        {
+            //switch (opp)
+            //{
+            //    case 1:
+            //        Result = Firstnumber + SecondNumber;
+            //        textResult.Text = Result.ToString();
+            //        break;
+            //    case 2:
+            //        Result = Firstnumber / SecondNumber;
+            //        textResult.Text = Result.ToString();
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+        }
+
+      
+    }
+}
